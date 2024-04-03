63cc9f20fa77744539627016db58cd6ecdc57d04
Sarath S S <98579610+sarathss100@users.noreply.github.com>
2024-04-03 19:05:58 +0530

    Add files via upload

---
+++ A9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int limit, i, j;
+  printf("Please enter a number : ");
+  scanf("%d", &limit);
+  
+  for(i = 1; i <= limit; i++) {
+    for(j = 1; j <= i; j++) {
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+  
+  return EXIT_SUCCESS;
+}
